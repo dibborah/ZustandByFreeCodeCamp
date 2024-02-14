1efeca5905ee072ffa7168f94ad9a04d7cfead84
dibborah <dibyajyotiborah469@gmail.com>
2024-02-14 11:55:10 +0530

    some files are just commented

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,12 +1,12 @@
-import { useStore } from "zustand";
+// import { useStore } from "zustand";
 import "./Column.css";
 import Task from "./Task";
 
 // eslint-disable-next-line react/prop-types
 const Column = ({ state }) => {
-    const tasks = useStore((store)=>{
-        store.tasks.filter((task)=>task.state === state);
-    })
+    // const tasks = useStore((store)=>{
+    //     store.tasks.filter((task)=>task.state === state);
+    // })
   return (
     <div className="column">
       <p>{state}</p>
