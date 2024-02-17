29f02a6c940282da25245abfe9a01fa3d5cc3b32
dibborah <dibyajyotiborah469@gmail.com>
2024-02-17 19:41:57 +0530

    deleteTask method added in store also on the application. Trash Icon is also added on the application

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,17 +2,19 @@
 import classNames from "classnames";
 import "./Task.css";
 import { useStore } from "../store";
+import Trash from "../assets/trash-2.svg"
 
 const Task = ({ title }) => {
-  const tasks = useStore((store)=> 
-  store.tasks.find((task)=> task.title === title)
-  )
+  const tasks = useStore((store) =>
+    store.tasks.find((task) => task.title === title)
+  );
+  const deleteTask = useStore((store) => store.deleteTask);
   return (
-    <div className="task">
+    <div className="task" draggable>
       <div>{tasks.title}</div>
       <div className="bottomWrapper">
-      <div></div>
-      <div className={classNames("status", tasks.state)}>{tasks.state}</div>
+        <div onClick={()=>deleteTask(tasks.title)}><img src={Trash}/></div>
+        <div className={classNames("status", tasks.state)}>{tasks.state}</div>
       </div>
     </div>
   );
