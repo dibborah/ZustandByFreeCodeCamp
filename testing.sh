b1ba7ef08506a67548b42a7768176f17e1773637
dibborah <dibyajyotiborah469@gmail.com>
2024-02-18 21:35:03 +0530

    starting to test zustand on dev

---
+++ testing.sh
@@ -0,0 +1,6 @@
+npm i --save-dev vitest @testing-library/jest-dom @testing-library/react jsdom
+
+# This is a shell script file so the .sh extension
+# Pound symbol is  used for writting comments in shell script files
+# In this files commands are written which are used or to be excuted in the application
+
