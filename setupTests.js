9e8c7ebdd08bfe51f05bde0a6bff042e05e53d72
dibborah <dibyajyotiborah469@gmail.com>
2024-02-19 20:35:39 +0530

    started the testing of zustand but didn't complete it

--- setupTests.js
+++ setupTests.js
@@ -1,1 +1,8 @@
 import {expects, afterEach} from 'vitest';
+import { cleanup } from '@testing-library/react';
+import matchers from '@testing-library/jest-dom/types/matchers';
+import '@testing-library/jest-dom';
+
+expects.extend(matchers);
+
+afterEach(cleanup);
